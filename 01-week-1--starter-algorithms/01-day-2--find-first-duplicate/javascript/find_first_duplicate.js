2d2099b82c3c5e8d3fbc71477b0f6c941f49bb8d
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-09-05 13:28:57 -0700

    Completed JavaScript code

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,6 +1,31 @@
 function findFirstDuplicate(arr) {
   // type your code here
+
+  //Set object does not allow duplicates so if you add a number to set that exit it would fail
+  const uniques = new Set()
+
+  for (let i = 0; i < arr.length; i++){
+    if (uniques.has(arr[i])) {
+      return arr[i];
+    }
+    uniques.add(arr[i])
+  }
+
+  return -1
 }
+
+
+// const uniques = new Set()
+
+// for (const value of arr){
+//   if (uniques.has(value)) {
+//     return value;
+//   }
+//   uniques.add(value)
+// }
+
+// return -1
+// }
 
 if (require.main === module) {
   // add your own tests in here
@@ -16,4 +41,8 @@ if (require.main === module) {
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
+
+//1. Loop through each item in array
+  // 2. Hold value of the loop and compare to against it
+
 // And a written explanation of your solution
