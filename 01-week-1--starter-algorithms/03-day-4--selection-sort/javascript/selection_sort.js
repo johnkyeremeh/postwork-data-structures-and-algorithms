1d5d66af7308879614d9a479f4cc4f8e992af910
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-09-08 18:45:02 -0700

    Completed selection_sort

--- 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
+++ 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
@@ -1,11 +1,29 @@
 function selectionSort(arr) {
   // type your code here
+
+  //[10, 4, 3, 2, 1, 5]
+  console.log("original value", arr)
+  for (i = 0; i < arr.length - 1; i++){
+    let minIndex = i
+    for (j = i + 1; j < arr.length; j++){
+      if (arr[j] < arr[minIndex]){      
+          minIndex = j            
+      }
+    }
+    let temp = arr[i]             
+    arr[i] = arr[minIndex]        
+    arr[minIndex] = temp 
+  }
+
+  console.log("updated", arr)
+  return arr
+
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [-1, 2, 3, 5]");
-  console.log("=>", selectionSort([3, -1, 5, 2]));
+  console.log("=>", selectionSort([10, 4, 3, 2, 1, 5]));
 
   console.log("");
 
@@ -21,3 +39,15 @@ module.exports = selectionSort;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+
+//Iterate through each element 
+  //create a min variable to hold min element and set it to
+  //compare element i with element element[i + 1]
+  //if elem > i + 1
+  // //create a variable to store as placeholder
+     //set the first element value to the value of the second element 
+     //set the second element index equal to the value of the first elmenent
+
+
+     
