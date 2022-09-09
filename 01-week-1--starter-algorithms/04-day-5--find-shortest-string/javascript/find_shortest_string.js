0b098f7d4e828c76a90e9a6fbdad6ace161034ec
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-09-08 19:26:05 -0700

    Completed assignment

--- 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
+++ 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
@@ -1,6 +1,42 @@
 function findShortestString(arr) {
-  // type your code here
+
+  let shortest = arr[0]
+
+  for (i = 0; i < arr.length - 1;  i++){
+    if (arr[i].length < shortest.length){
+      shortest  = arr[i];
+    }
+  }
+  console.log("output array", arr)
+  return arr[0]
+
+  /*
+
+  for (i = 0; i < arr.length - 1;  i++){
+    let shortest = i
+    for (j = i+1; j < arr.length; j++){
+      if (arr[j].length < arr[shortest].length) {
+        shortest = j
+      }
+    }
+
+        let temp = arr[i]
+        arr[i] = arr[shortest]
+        arr[shortest] = temp
+
+  }
+  console.log("output", arr)
+  return arr[0]
+
+
+
+  */
 }
+
+
+
+
+
 
 if (require.main === module) {
   // add your own tests in here
