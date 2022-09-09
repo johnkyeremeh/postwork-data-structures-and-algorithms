932d0f10a44c967703a70405e1fd154f22ebbf85
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-09-08 18:47:46 -0700

    Completed assignment

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
   // type your code here
+
+  // 0,1,1,2,3
+  //2
+  //fib[2] = 1
+  //fib[4] = 3
+  if (num <=1){
+    return num;
+  }
+  
+  const fib = [0, 1]
+
+  for (let i = 2; i <= num; i++){
+    fib[i] = fib[i - 2] + fib[i - 1]
+  }
+  //return the index of last digit of fib
+  return fib[fib.length - 1]
+
 }
 
 if (require.main === module) {
@@ -21,4 +38,8 @@ if (require.main === module) {
 module.exports = fibonacci;
 
 // Please add your pseudocode to this file
+
+// iterates through an array with Num elements where Num is the length of the array
+// create new index of array with each iteration equal to two indexes and 1 index
 // And a written explanation of your solution
+
